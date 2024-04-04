1cdfb8845408da009fa27a0358b9d2bcfd38f2f4
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2024-04-04 13:13:18 -0400

    skibidi sigma setup

---
+++ SQL/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+  import { createClient } from '@supabase/supabase-js'
+
+  export const supabase = createClient('https://uxerhlvwgipbpprpkypg.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4ZXJobHZ3Z2lwYnBwcnBreXBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIyNDg3MDgsImV4cCI6MjAyNzgyNDcwOH0.DoqI-guTVz674WKNj4i1W0qXKUDjmIlVaejopU8yQxk')
