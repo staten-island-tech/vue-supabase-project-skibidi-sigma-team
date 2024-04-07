7ae48b9799b6d3edb5832078f9ad0cde148f03e2
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2024-04-06 21:25:04 -0400

    monkeying around | still no clue on TS :angry:

--- SQL/src/components/TheWelcome.vue
+++ SQL/src/components/TheWelcome.vue
@@ -8,14 +8,27 @@ async function testhang() {
   const { data } = await supabase.from('Makerspace_test').select()
   Makerspace_test.value = data
 }
-
+/* 
+async function update() {
+  const { error } = await supabase
+  .from('Makerspace_test')
+  .update({ Amount: 2})
+  .eq('id', 2)
+  
+}
+ 
+This thing acctuallyy goes into the sql table and changes the value
+*/
 onMounted(() => {
   testhang()
+/* update() */
+ 
+ 
 })
 </script>
 
 <template>
   <ul>
-    <li v-for="thing in Makerspace_test" :key="thing.id">{{ thing.Item }}</li>
+    <li v-for="thing in Makerspace_test" :key="thing.id">{{ thing.Amount }}</li>
   </ul>
 </template>
