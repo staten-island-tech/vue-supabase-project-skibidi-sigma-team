0269d182cebced4f078b8dbd547effb8a9d148c4
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2024-04-12 13:13:14 -0400

    erm what the sigma

--- SQL/src/components/TIGER.vue
+++ SQL/src/components/TIGER.vue
@@ -1,9 +1,33 @@
-<script setup lang="ts">
+<script setup>
+
+
+import { supabase } from '../lib/supabaseClient'
+
+
+
+
+async function signUpNewUser() {
+  const { data, error } = await supabase.auth.signUp({
+    email: 'conplay55@gmail.com',
+    password: 'testtest',
+    
+  })
+}
+
+ 
+
 
 </script>
 
 <template>
-  <p>poop
-    
-  </p>
+  
+<label for="Email">Enter your email</label><br>
+<input type="text" id="Email" name="Email" ><br>
+
+<label for="Email">Make a password</label><br>
+<input type="text" id="PASSWORD" name="Email" ><br>
+<input type="submit" @click="console.log(test)"><br> <!-- collect the form data nad use it in sumbi funtion -->
+
+
+  <button  @click="signUpNewUser() , console.log('bozo lost his data')">SUMBIT DATA</button>
 </template>
