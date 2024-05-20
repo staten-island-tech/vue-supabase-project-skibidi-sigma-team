c8afe4509820bea9aa3619b1d02836eab7ada9c1
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2024-05-19 21:10:04 -0400

    Dev things to help see data

--- SQL/src/views/LOGIN_INTO.vue
+++ SQL/src/views/LOGIN_INTO.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import TIGER from '../components/TIGER.vue'
 import LION from '../components/LION.vue'
+import { StealData } from '../stores/USER_DATA';
+const somethingtest1 = StealData();
+
+
 </script>
 
 <template>
@@ -8,5 +12,6 @@ import LION from '../components/LION.vue'
     <TIGER />
     <LION />
   </main>
+  <p>EMAIL?: {{somethingtest1.email}}</p>
 </template>
 <!-- make login, show error messaghe -->
